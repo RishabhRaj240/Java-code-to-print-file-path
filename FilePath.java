4888d0f5f4e9c7a433d9962294e02475111c1775
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-15 18:16:14 +0530

    Add files via upload

---
+++ FilePath.java
@@ -0,0 +1,9 @@
+class FilePath {
+    public static void main(String[] args) {
+         //System.out.println("D:\abc\xyz\enames.txt"); //CE
+         System.out.println("D:\\abc\\xyz\\enames.txt");
+
+         System.out.println("D:\new\text\snames.txt"); // no CE
+         System.out.println("D:\\new\\text\\snames.txt");
+    }
+}
